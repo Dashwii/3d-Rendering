85b9a75c433a65bc8fdeafd4d9f4cd02eb802a63
Dashwii <dashwiibox@gmail.com>
2021-12-09 07:05:43 -0500

    Added perspective viewing

--- main.py
+++ main.py
@@ -1,124 +1,111 @@
 import pygame
-import math
+from math import sin, cos, tan, radians
 import copy
 import time
 
-pygame.init()
 WIDTH, HEIGHT = 1000, 1000
-pygame.display.set_caption("3D render")
+OFFSET = (WIDTH // 2, HEIGHT // 2)
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-BLUE = (0, 0, 255)
-BLACK = (0, 0, 0)
+pygame.display.set_caption("3D rendering")
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+scalar = 50
+rotation_theta = time.time()
+
+near = 0.1
+far = 1000
+aspect_ratio = WIDTH / HEIGHT
+fov = 1 / tan(radians(100 / 2))
+
+rot_x_speed = .3
+rot_y_speed = .1
+rot_z_speed = .2
+
+camera_cords = (0, 0, 0)
 
 
-f_near = 0.1
-f_far = 1000
-fov = 90
-aspect_ratio = HEIGHT / WIDTH
-fov_rad = 1 / math.tan(math.radians(fov * 0.5))
-
-
-projection_matrix = [[0.0 for _ in range(4)] for _ in range(4)]
-projection_matrix[0][0] = aspect_ratio * fov_rad
-projection_matrix[1][1] = fov_rad
-projection_matrix[2][2] = f_far / (f_far - f_near)
-projection_matrix[3][2] = (-f_far * f_near) / (f_far - f_near)
-projection_matrix[2][3] = 1
-projection_matrix[3][3] = 0.0
-
-
-def multiply_matrix(vector, matrix_b):
-    output_x = vector.x * matrix_b[0][0] + vector.y * matrix_b[1][0] + vector.z * matrix_b[2][0] + matrix_b[3][0]
-    output_y = vector.x * matrix_b[0][1] + vector.y * matrix_b[1][1] + vector.z * matrix_b[2][1] + matrix_b[3][1]
-    output_z = vector.x * matrix_b[0][2] + vector.y * matrix_b[1][2] + vector.z * matrix_b[2][2] + matrix_b[3][2]
-    w = vector.x * matrix_b[0][3] + vector.y * matrix_b[1][3] + vector.z * matrix_b[2][3] + matrix_b[3][3]
-
-    if w:
-        output_x = output_x / w
-        output_y = output_y / w
-        output_z = output_z / w
-    return Vec3d(output_x, output_y, output_z)
-
-
-class Matrix:
-    def __init__(self):
-        pass
-
-
-class Vec3d:
-    def __init__(self, x=0, y=0, z=0, w=1):
+class Vector:
+    def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
+        self.vector_list = [[self.x], [self.y], [self.z]]
 
 
 class Triangle:
-    def __init__(self, v1=None, v2=None, v3=None, color=WHITE):
-        self.v1 = v1
-        self.v2 = v2
-        self.v3 = v3
+    def __init__(self, points_1, points_2, points_3, color):
         self.color = color
+        self.v1 = Vector(points_1[0], points_1[1], points_1[2])
+        self.v2 = Vector(points_2[0], points_2[1], points_2[2])
+        self.v3 = Vector(points_3[0], points_3[1], points_3[2])
+        self.vectors = (self.v1, self.v2, self.v3)
 
 
-class MeshCube:
+class Cube:
     def __init__(self):
-        self.triangles = [
-            # SOUTH
-            Triangle(Vec3d(0, 0, 0), Vec3d(0, 1, 0), Vec3d(1, 1, 0)),
-            Triangle(Vec3d(0, 0, 0), Vec3d(1, 1, 0), Vec3d(1, 0, 0)),
+        self.mesh = [
+            # NORTH
+            Triangle((-1.0, 1.0,  -1.0), (1.0, -1.0, -1.0), (-1.0,  -1.0,  -1.0), WHITE),
+            Triangle((-1.0, 1.0,  -1.0), (1.0, 1.0,  -1.0), (1.0,   -1.0,  -1.0), WHITE),
 
             # EAST
-            Triangle(Vec3d(1, 0, 0), Vec3d(1, 1, 0), Vec3d(1, 1, 1)),
-            Triangle(Vec3d(1, 0, 0), Vec3d(1, 1, 1), Vec3d(1, 0, 1)),
+            Triangle((1.0,  1.0,  -1.0), (1.0, -1.0, 1.0), (1.0, -1.0, -1.0), WHITE),
+            Triangle((1.0,  1.0,  -1.0), (1.0, 1.0,  1.0), (1.0, -1.0,  1.0), WHITE),
 
-            # NORTH
-            Triangle(Vec3d(1, 0, 1), Vec3d(1, 1, 1), Vec3d(0, 1, 1)),
-            Triangle(Vec3d(1, 0, 1), Vec3d(0, 1, 1), Vec3d(0, 0, 1)),
+            # SOUTH
+            Triangle((1.0,  1.0,  1.0), (-1.0, -1.0, 1.0), (1.0,  -1.0,  1.0), WHITE),
+            Triangle((1.0,  1.0,  1.0), (-1.0, 1.0,  1.0), (-1.0, -1.0, 1.0), WHITE),
 
             # WEST
-            Triangle(Vec3d(0, 0, 1), Vec3d(0, 1, 1), Vec3d(0, 1, 0)),
-            Triangle(Vec3d(0, 0, 1), Vec3d(0, 1, 0), Vec3d(0, 0, 0)),
+            Triangle((-1.0, 1.0,  1.0), (-1.0, -1.0, -1.0), (-1.0, -1.0,  1.0), WHITE),
+            Triangle((-1.0, 1.0,  1.0), (-1.0, 1.0,  -1.0), (-1.0, -1.0, -1.0), WHITE),
 
             # TOP
-            Triangle(Vec3d(0, 1, 0), Vec3d(0, 1, 1), Vec3d(1, 1, 1)),
-            Triangle(Vec3d(0, 1, 0), Vec3d(1, 1, 1), Vec3d(1, 1, 0)),
+            Triangle((-1.0, 1.0,  1.0), (1.0,  1.0, -1.0), (-1.0, 1.0, -1.0), WHITE),
+            Triangle((-1.0, 1.0,  1.0), (1.0,  1.0,  1.0), (1.0,  1.0, -1.0), WHITE),
 
             # BOTTOM
-            Triangle(Vec3d(1, 0, 1), Vec3d(0, 0, 1), Vec3d(0, 0, 0)),
-            Triangle(Vec3d(1, 0, 1), Vec3d(0, 0, 0), Vec3d(1, 0, 0))
+            Triangle((-1.0, -1.0,  -1.0), (1.0,  -1.0, 1.0), (1.0, -1.0, 1.0), WHITE),
+            Triangle((-1.0, -1.0,  -1.0), (-1.0, -1.0, 1.0), (1.0,  -1.0,  1.0), WHITE)
         ]
 
 
-def rotate_triangle(triangle):
-    matrix_rot_z[0][0] = math.cos(theta)
-    matrix_rot_z[0][1] = math.sin(theta)
-    matrix_rot_z[1][0] = -(math.sin(theta))
-    matrix_rot_z[1][1] = math.cos(theta)
-    matrix_rot_z[2][2] = 1
-    matrix_rot_z[3][3] = 1
+def matrix_multiplication(a, b):
+    columns_a = len(a[0])
+    rows_a = len(a)
+    columns_b = len(b[0])
+    rows_b = len(b)
 
-    matrix_rot_x[0][0] = 1
-    matrix_rot_x[1][1] = math.cos(theta * 0.5)
-    matrix_rot_x[1][2] = math.sin(theta * 0.5)
-    matrix_rot_x[2][1] = -(math.sin(theta * 0.5))
-    matrix_rot_x[2][2] = math.cos(theta * 0.5)
-    matrix_rot_x[3][3] = 1
+    result_matrix = [[j for j in range(columns_b)] for i in range(rows_a)]
+    if columns_a == rows_b:
+        for x in range(rows_a):
+            for y in range(columns_b):
+                sum = 0
+                for k in range(columns_a):
+                    sum += a[x][k] * b[k][y]
+                result_matrix[x][y] = sum
+        return result_matrix
 
-    tri_rotated_z = Triangle(
-        multiply_matrix(triangle.v1, matrix_rot_z),
-        multiply_matrix(triangle.v2, matrix_rot_z),
-        multiply_matrix(triangle.v3, matrix_rot_z),
-        color=triangle.color
-    )
-    tri_rotated_x = Triangle(
-        multiply_matrix(tri_rotated_z.v1, matrix_rot_x),
-        multiply_matrix(tri_rotated_z.v2, matrix_rot_x),
-        multiply_matrix(tri_rotated_z.v3, matrix_rot_x),
-        color=tri_rotated_z.color
-    )
-    return tri_rotated_x
+    else:
+        print("Columns of the first matrix must be equal to the rows of the second matrix")
+        return None
+
+
+def multiply_vector(vector, matrix_b):
+    out_x = vector.x * matrix_b[0][0] + vector.y * matrix_b[1][0] + vector.z * matrix_b[2][0]
+    out_y = vector.x * matrix_b[0][1] + vector.y * matrix_b[1][1] + vector.z * matrix_b[2][1]
+    out_z = vector.x * matrix_b[0][2] + vector.y * matrix_b[1][2] + vector.z * matrix_b[2][2]
+    return out_x, out_y, out_z
+
+
+def scale(triangle):
+    scaled_axis = [[], [], []]
+    for index, vector in enumerate([triangle.v1, triangle.v2, triangle.v3]):
+        scaled_axis[index].append(vector.x * scalar + OFFSET[0])
+        scaled_axis[index].append(vector.y * -scalar + OFFSET[1])
+        scaled_axis[index].append(vector.z)
+    return Triangle((scaled_axis[0][0], scaled_axis[0][1], scaled_axis[0][2]), (scaled_axis[1][0], scaled_axis[1][1], scaled_axis[1][2]), (scaled_axis[2][0], scaled_axis[2][1], scaled_axis[2][1]))
 
 
 def draw_triangle(x_1, y_1, x_2, y_2, x_3, y_3, color):
@@ -127,62 +114,52 @@ def draw_triangle(x_1, y_1, x_2, y_2, x_3, y_3, color):
     pygame.draw.line(WINDOW, color, (x_3, y_3), (x_1, y_1))
 
 
-def draw_stuff():
+def blit():
+    rot_z_matrix = [[cos(rotation_theta * rot_z_speed), -sin(rotation_theta * rot_z_speed), 0],
+                    [sin(rotation_theta * rot_z_speed), cos(rotation_theta * rot_z_speed), 0],
+                    [0, 0, 1]]
+
+    rot_x_matrix = [[1, 0, 0],
+                    [0, cos(rotation_theta * rot_x_speed), -sin(rotation_theta * rot_x_speed)],
+                    [0, sin(rotation_theta * rot_x_speed), cos(rotation_theta * rot_x_speed)]]
+
+    rot_y_matrix = [[cos(rotation_theta * rot_y_speed), 0, -sin(rotation_theta * rot_y_speed)],
+                    [0, 1, 0],
+                    [sin(rotation_theta * rot_y_speed), 0, cos(rotation_theta * rot_y_speed)]]
     WINDOW.fill(BLACK)
-    for triangle in mesh_cube.triangles:
-        copied_tri = copy.deepcopy(triangle)
-        tri_rotated = rotate_triangle(copied_tri)
+    for triangle in square.mesh:
+        triangle_copy = copy.deepcopy(triangle)
+        for vector in triangle_copy.vectors:
+            rotated_2d = matrix_multiplication(rot_z_matrix, vector.vector_list)
+            rotated_2d = matrix_multiplication(rot_x_matrix, rotated_2d)
+            rotated_2d = matrix_multiplication(rot_y_matrix, rotated_2d)
+            rotated_2d.append([1])
 
-        tri_translated = tri_rotated
+            distance = 3
+            z = 1 / (distance - rotated_2d[2][0])
+            projection_matrix = [[aspect_ratio * fov, 0, 0, 0],
+                                 [0, fov, 0, 0],
+                                 [0, 0, distance / (distance - rotated_2d[2][0]), 1],
+                                 [0, 0, 0, -(distance * rotated_2d[2][0]) / (distance - rotated_2d[2][0])]]
 
-        tri_translated.v1.z = triangle.v1.z + 2
-        tri_translated.v2.z = triangle.v2.z + 2
-        tri_translated.v3.z = triangle.v3.z + 2
+            vector.vector_list[2][0] += 3
+            rotated_2d[0][0] /= z
+            rotated_2d[1][0] /= z
+            projected_2d = matrix_multiplication(projection_matrix, rotated_2d)
+            vector.vector_list[0][0] = int(projected_2d[0][0] * scalar) + OFFSET[0]
+            vector.vector_list[1][0] = int(projected_2d[1][0] * scalar) + OFFSET[1]
 
-        tri_projected = Triangle(
-            multiply_matrix(tri_translated.v1, projection_matrix),
-            multiply_matrix(tri_translated.v2, projection_matrix),
-            multiply_matrix(tri_translated.v3, projection_matrix),
-            color=tri_translated.color
-        )
-
-        tri_projected.v1.x += 1
-        tri_projected.v1.y += 1
-        tri_projected.v2.x += 1
-        tri_projected.v2.y += 1
-        tri_projected.v3.x += 1
-        tri_projected.v3.y += 1
-
-        tri_projected.v1.x *= 0.5 * WIDTH
-        tri_projected.v1.y *= 0.5 * WIDTH
-        tri_projected.v2.x *= 0.5 * WIDTH
-        tri_projected.v2.y *= 0.5 * WIDTH
-        tri_projected.v3.x *= 0.5 * WIDTH
-        tri_projected.v3.y *= 0.5 * WIDTH
-
-        draw_triangle(tri_projected.v1.x, tri_projected.v1.y, tri_projected.v2.x, tri_projected.v2.y, tri_projected.v3.x, tri_projected.v3.y, tri_projected.color)
-
-    pygame.display.flip()
+            pygame.draw.circle(WINDOW, WHITE, (vector.vector_list[0][0], vector.vector_list[1][0]), 10)
+        draw_triangle(triangle_copy.v1.vector_list[0][0], triangle_copy.v1.vector_list[1][0],
+                      triangle_copy.v2.vector_list[0][0], triangle_copy.v2.vector_list[1][0],
+                      triangle_copy.v3.vector_list[0][0], triangle_copy.v3.vector_list[1][0], WHITE)
+    pygame.display.update()
 
 
-mesh_cube = MeshCube()
-elapsed_time = time.time()
-theta = 0
-matrix_rot_z = [[0. for _ in range(4)] for _ in range(4)]
-matrix_rot_x = [[0. for _ in range(4)] for _ in range(4)]
-
-rotate_event = pygame.USEREVENT + 0
-pygame.time.set_timer(rotate_event, 1)
-
+square = Cube()
 while True:
-    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.display.quit()
-        if event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_ESCAPE:
-                pygame.display.quit()
-        if event.type == rotate_event:
-            theta = 1 * elapsed_time
-    draw_stuff()
-    elapsed_time = time.time()
+    blit()
+    rotation_theta = time.time()
